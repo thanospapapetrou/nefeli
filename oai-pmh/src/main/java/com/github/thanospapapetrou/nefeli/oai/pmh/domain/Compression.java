f2ee289fae8e4de05b13a1119944efce29a1ad2a
thanos <thanos.papapetrou@gmail.com>
2016-03-13 20:45:06 +0200

    Added protocolVersion and Compression

---
+++ oai-pmh/src/main/java/com/github/thanospapapetrou/nefeli/oai/pmh/domain/Compression.java
@@ -0,0 +1,45 @@
+package com.github.thanospapapetrou.nefeli.oai.pmh.domain;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * Enumeration defining a <code>compression</code> OAI-PMH element.
+ * 
+ * @author thanos
+ * @see <a href="http://www.ietf.org/rfc/rfc2616.txt">RFC 2616 Hypertext Transfer Protocol -- HTTP/1.1</a>
+ */
+@XmlEnum
+@XmlType(name = "string", namespace = XMLConstants.W3C_XML_SCHEMA_NS_URI)
+public enum Compression {
+	/**
+	 * the gzip encoding format (<code>gzip</code>)
+	 */
+	@XmlEnumValue("gzip") GZIP,
+
+	/**
+	 * the compress encoding format (<code>compress</code>)
+	 */
+	@XmlEnumValue("compress") COMPRESS,
+
+	/**
+	 * the deflate encoding format (<code>deflate</code>)
+	 */
+	@XmlEnumValue("deflate") DEFLATE,
+
+	/**
+	 * the default (identity) encoding format (<code>identity</code>)
+	 */
+	@XmlEnumValue("identity") IDENTITY;
+
+	@Override
+	public String toString() {
+		try {
+			return getClass().getDeclaredField(name()).getAnnotation(XmlEnumValue.class).value();
+		} catch (final NoSuchFieldException e) {
+			return null;
+		}
+	}
+}
---
+++ oai-pmh/src/main/java/com/github/thanospapapetrou/nefeli/oai/pmh/domain/ProtocolVersion.java
@@ -0,0 +1,28 @@
+package com.github.thanospapapetrou.nefeli.oai.pmh.domain;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * Enumeration defining a <code>protocolVersion</code> OAI-PMH element.
+ * 
+ * @author thanos
+ */
+@XmlEnum
+@XmlType(name = "protocolVersionType")
+public enum ProtocolVersion {
+	/**
+	 * OAI-PMH 2.0 (<code>2.0</code>)
+	 */
+	@XmlEnumValue("2.0") OAI_PMH_2_0;
+
+	@Override
+	public String toString() {
+		try {
+			return getClass().getDeclaredField(name()).getAnnotation(XmlEnumValue.class).value();
+		} catch (final NoSuchFieldException e) {
+			return null;
+		}
+	}
+}
