51f9cfbfcf591f77707894168e2d2a0135253227
thanos <thanos.papapetrou@gmail.com>
2016-03-19 00:55:07 +0200

    Cleaned up request

---
+++ oai-pmh/src/main/java/com/github/thanospapapetrou/nefeli/oai/pmh/domain/Request.java
@@ -0,0 +1,387 @@
+package com.github.thanospapapetrou.nefeli.oai.pmh.domain;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+/**
+ * Class representing a <code>request</code> OAI-PMH element.
+ * 
+ * @author thanos
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "requestType", propOrder = {"baseUrl"})
+public class Request {
+	@XmlAttribute(name = "verb")
+	@XmlSchemaType(name = "verbType", namespace = OaiPmh.NAMESPACE)
+	private final Verb verb;
+
+	@XmlAttribute(name = "identifier")
+	@XmlSchemaType(name = "identifierType", namespace = OaiPmh.NAMESPACE)
+	private final URI identifier;
+
+	@XmlAttribute(name = "metadataPrefix")
+	@XmlSchemaType(name = "metadataPrefixType", namespace = OaiPmh.NAMESPACE)
+	private final MetadataPrefix metadataPrefix;
+
+	@XmlAttribute(name = "from")
+	@XmlSchemaType(name = "UTCdatetimeType", namespace = OaiPmh.NAMESPACE)
+	private final Date from;
+
+	@XmlAttribute(name = "until")
+	@XmlSchemaType(name = "UTCdatetimeType", namespace = OaiPmh.NAMESPACE)
+	private final Date until;
+
+	@XmlAttribute(name = "set")
+	@XmlSchemaType(name = "setSpecType", namespace = OaiPmh.NAMESPACE)
+	private final SetSpec set;
+
+	@XmlAttribute(name = "resumptionToken")
+	@XmlSchemaType(name = "string")
+	private final String resumptionToken;
+
+	@XmlValue
+	@XmlSchemaType(name = "anyURI")
+	private final URL baseUrl;
+
+	/**
+	 * Construct a new <code>request<code> element corresponding to a <code>Identify</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @return a new <code>request</code> element
+	 */
+	public static Request identify(final URL baseUrl) {
+		return new Request(Verb.IDENTIFY, null, null, null, null, null, null, baseUrl);
+	}
+
+	/**
+	 * Construct a new <code>request<code> element possibly with an <code>identifier</code> attribute corresponding to a <code>ListMetadataFormats</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param identifier
+	 *            the <code>identifier</code> attribute or <code>null</code> to leave it unspecified
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listMetadataFormats(final URL baseUrl, final URI identifier) {
+		return new Request(Verb.LIST_METADATA_FORMATS, identifier, null, null, null, null, null, baseUrl);
+	}
+
+	/**
+	 * Construct a new <code>request<code> element corresponding to a <code>ListMetadataFormats</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listMetadataFormats(final URL baseUrl) {
+		return listMetadataFormats(baseUrl, null);
+	}
+
+	/**
+	 * Construct a new <code>request<code> element corresponding to a <code>ListSets</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listSets(final URL baseUrl) {
+		return listSets(baseUrl, null);
+	}
+
+	/**
+	 * Construct a new <code>request<code> element with a <code>resumptionToken</code> attribute corresponding to a <code>ListSets</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param resumptionToken
+	 *            the <code>resumptionToken</code> attribute
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listSets(final URL baseUrl, final String resumptionToken) {
+		return new Request(Verb.LIST_SETS, null, null, null, null, null, Objects.requireNonNull(resumptionToken, "Resumption token must not be null"), baseUrl);
+	}
+
+	/**
+	 * Construct a new <code>request<code> element with a <code>metadataPrefix</code> attribute and possibly <code>from</code>, <code>until</code> and <code>set</code> attributes corresponding to a <code>ListIdentifiers</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> attribute
+	 * @param from
+	 *            the <code>from</code> attribute or <code>null</code> to leave it unspecified
+	 * @param until
+	 *            the <code>until</code> attribute or <code>null</code> to leave it unspecified
+	 * @param set
+	 *            the <code>set</code> attribute or <code>null</code> to leave it unspecified
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listIdentifiers(final URL baseUrl, final MetadataPrefix metadataPrefix, final Date from, final Date until, final SetSpec set) {
+		return new Request(Verb.LIST_IDENTIFIERS, null, Objects.requireNonNull(metadataPrefix, "Metadata prefix must not be null"), from, until, set, null, baseUrl);
+	}
+
+	/**
+	 * Construct a new <code>request<code> element with a <code>metadataPrefix</code> attribute and possibly <code>from</code> and <code>until</code> attributes corresponding to a <code>ListIdentifiers</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> attribute
+	 * @param from
+	 *            the <code>from</code> attribute or <code>null</code> to leave it unspecified
+	 * @param until
+	 *            the <code>until</code> attribute or <code>null</code> to leave it unspecified
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listIdentifiers(final URL baseUrl, final MetadataPrefix metadataPrefix, final Date from, final Date until) {
+		return listIdentifiers(baseUrl, metadataPrefix, from, until, null);
+	}
+
+	/**
+	 * Construct a new <code>request<code> element with a <code>metadataPrefix</code> and possibly a <code>set</code> attribute corresponding to a <code>ListIdentifiers</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> attribute
+	 * @param set
+	 *            the <code>set</code> attribute or <code>null</code> to leave it unspecified
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listIdentifiers(final URL baseUrl, final MetadataPrefix metadataPrefix, final SetSpec set) {
+		return listIdentifiers(baseUrl, metadataPrefix, null, null, set);
+	}
+
+	/**
+	 * Construct a new <code>request<code> element with a <code>metadataPrefix</code> attribute corresponding to a <code>ListIdentifiers</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> attribute
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listIdentifiers(final URL baseUrl, final MetadataPrefix metadataPrefix) {
+		return listIdentifiers(baseUrl, metadataPrefix, null);
+	}
+
+	/**
+	 * Construct a new <code>request<code> element with a <code>resumptionToken</code> attribute corresponding to a <code>ListIdentifiers</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param resumptionToken
+	 *            the <code>resumptionToken</code> attribute
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listIdentifiers(final URL baseUrl, final String resumptionToken) {
+		return new Request(Verb.LIST_IDENTIFIERS, null, null, null, null, null, Objects.requireNonNull(resumptionToken, "Resumption token must not be null"), baseUrl);
+	}
+
+	/**
+	 * Construct a new <code>request</code> element with a <code>metadataPrefix</code> attribute and possibly <code>from</code>, <code>until</code> and <code>set</code> attributes corresponding to a <code>ListRecords</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> attribute
+	 * @param from
+	 *            the <code>from</code> attribute or <code>null</code> to leave it unspecified
+	 * @param until
+	 *            the <code>until</code> attribute or <code>null</code> to leave it unspecified
+	 * @param set
+	 *            the <code>set</code> attribute or <code>null</code> to leave it unspecified
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listRecords(final URL baseUrl, final MetadataPrefix metadataPrefix, final Date from, final Date until, final SetSpec set) {
+		return new Request(Verb.LIST_RECORDS, null, Objects.requireNonNull(metadataPrefix, "Metadata prefix must not be null"), from, until, set, null, baseUrl);
+	}
+
+	/**
+	 * Construct a new <code>request</code> element with a <code>metadataPrefix</code> attribute and possibly <code>from</code> and <code>until</code> attributes corresponding to a <code>ListRecords</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> attribute
+	 * @param from
+	 *            the <code>from</code> attribute or <code>null</code> to leave it unspecified
+	 * @param until
+	 *            the <code>until</code> attribute or <code>null</code> to leave it unspecified
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listRecords(final URL baseUrl, final MetadataPrefix metadataPrefix, final Date from, final Date until) {
+		return listRecords(baseUrl, metadataPrefix, from, until, null);
+	}
+
+	/**
+	 * Construct a new <code>request</code> element with a <code>metadataPrefix</code> attribute and possibly a <code>set</code> attribute corresponding to a <code>ListRecords</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> attribute
+	 * @param set
+	 *            the <code>set</code> attribute or <code>null</code> to leave it unspecified
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listRecords(final URL baseUrl, final MetadataPrefix metadataPrefix, final SetSpec set) {
+		return listRecords(baseUrl, metadataPrefix, null, null, set);
+	}
+
+	/**
+	 * Construct a new <code>request</code> element with a <code>metadataPrefix</code> attribute corresponding to a <code>ListRecords</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> attribute
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listRecords(final URL baseUrl, final MetadataPrefix metadataPrefix) {
+		return listRecords(baseUrl, metadataPrefix, null);
+	}
+
+	/**
+	 * Construct a new <code>request</code> element with a <code>resumptionToken</code> attribute corresponding to a <code>ListRecords</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param resumptionToken
+	 *            the <code>resumptionToken</code> attribute
+	 * @return a new <code>request</code> element
+	 */
+	public static Request listRecords(final URL baseUrl, final String resumptionToken) {
+		return new Request(Verb.LIST_RECORDS, null, null, null, null, null, Objects.requireNonNull(resumptionToken, "Resumption token must not be null"), baseUrl);
+	}
+
+	/**
+	 * Construct a new <code>request</code> element corresponding to a <code>GetRecord</code> OAI-PMH request.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @param identifier
+	 *            the <code>identifier</code> attribute
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> attribute
+	 * @return a new <code>request</code> element
+	 */
+	public static Request getRecord(final URL baseUrl, final URI identifier, final MetadataPrefix metadataPrefix) {
+		return new Request(Verb.GET_RECORD, Objects.requireNonNull(identifier, "Identifier must not be null"), Objects.requireNonNull(metadataPrefix, "Metadata prefix must not be null"), null, null, null, null, baseUrl);
+	}
+
+	/**
+	 * Construct a new <code>request</code> element corresponding to a <code>badVerb</code> or a <code>badArgument</code> OAI-PMH error.
+	 * 
+	 * @param baseUrl
+	 *            the base URL
+	 * @return a new <code>request</code> element
+	 */
+	public static Request error(final URL baseUrl) {
+		return new Request(null, null, null, null, null, null, null, baseUrl);
+	}
+
+	private Request(final Verb verb, final URI identifier, final MetadataPrefix metadataPrefix, final Date from, final Date until, final SetSpec set, final String resumptionToken, final URL baseUrl) {
+		this.verb = verb;
+		this.identifier = identifier;
+		this.metadataPrefix = metadataPrefix;
+		this.from = from;
+		this.until = until;
+		this.set = set;
+		this.resumptionToken = resumptionToken;
+		this.baseUrl = Objects.requireNonNull(baseUrl, "Base URL must not be null");
+	}
+
+	private Request() {
+		verb = null;
+		identifier = null;
+		metadataPrefix = null;
+		from = null;
+		until = null;
+		set = null;
+		resumptionToken = null;
+		baseUrl = null;
+	}
+
+	/**
+	 * Get the <code>verb</code> attribute.
+	 * 
+	 * @return the <code>verb</code> attribute or <code>null</code> if unspecified
+	 */
+	public Verb getVerb() {
+		return verb;
+	}
+
+	/**
+	 * Get the <code>identifier</code> attribute.
+	 * 
+	 * @return the <code>identifier</code> attribute or <code>null</code> if unspecified
+	 */
+	public URI getIdentifier() {
+		return identifier;
+	}
+
+	/**
+	 * Get the <code>metadataPrefix</code> attribute.
+	 * 
+	 * @return the <code>metadataPrefix</code> attribute or <code>null</code> if unspecified
+	 */
+	public MetadataPrefix getMetadataPrefix() {
+		return metadataPrefix;
+	}
+
+	/**
+	 * Get the <code>from</code> attribute.
+	 * 
+	 * @return the <code>from</code> attribute or <code>null</code> if unspecified
+	 */
+	public Date getFrom() {
+		return from;
+	}
+
+	/**
+	 * Get the <code>until</code> attribute.
+	 * 
+	 * @return the <code>until</code> attribute or <code>null</code> if unspecified
+	 */
+	public Date getUntil() {
+		return until;
+	}
+
+	/**
+	 * Get the <code>set</code> attribute.
+	 * 
+	 * @return the <code>set</code> attribute or <code>null</code> if unspecified
+	 */
+	public SetSpec getSet() {
+		return set;
+	}
+
+	/**
+	 * Get the <code>resumptionToken</code> attribute.
+	 * 
+	 * @return the <code>resumptionToken</code> attribute or <code>null</code> if unspecified
+	 */
+	public String getResumptionToken() {
+		return resumptionToken;
+	}
+
+	/**
+	 * Get the base URL.
+	 * 
+	 * @return the base URL
+	 */
+	public URL getBaseUrl() {
+		return baseUrl;
+	}
+}
