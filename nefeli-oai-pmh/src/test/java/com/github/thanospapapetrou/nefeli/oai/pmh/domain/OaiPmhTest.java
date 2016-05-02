4006e99f5aa1c2a982fd037af2f46b38a956b8c0
thanos <thanos.papapetrou@gmail.com>
2016-05-02 18:02:27 +0300

    Added granularity in tests

--- nefeli-oai-pmh/src/test/java/com/github/thanospapapetrou/nefeli/oai/pmh/domain/OaiPmhTest.java
+++ nefeli-oai-pmh/src/test/java/com/github/thanospapapetrou/nefeli/oai/pmh/domain/OaiPmhTest.java
@@ -2,13 +2,13 @@ package com.github.thanospapapetrou.nefeli.oai.pmh.domain;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.util.Arrays;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -21,11 +21,34 @@ import org.testng.annotations.Test;
  */
 @Test
 public class OaiPmhTest {
-	private static final String EXAMPLES = "/com/github/thanospapapetrou/nefeli/oai/pmh/examples";
+	private static final Map<String, Granularity> EXAMPLES = new HashMap<String, Granularity>() {
+		private static final long serialVersionUID = 0L;
+
+		{
+			put("Error1", Granularity.SECONDS);
+			put("Error2", Granularity.SECONDS);
+			put("GetRecord1", Granularity.DAY);
+			put("GetRecord2", Granularity.SECONDS);
+			put("GetRecord3", Granularity.SECONDS);
+			put("Identify", Granularity.SECONDS);
+			put("ListIdentifiers1", Granularity.DAY);
+			put("ListIdentifiers2", Granularity.DAY);
+			put("ListIdentifiers3", Granularity.SECONDS);
+			put("ListMetadataFormats1", Granularity.SECONDS);
+			put("ListMetadataFormats2", Granularity.SECONDS);
+			put("ListMetadataFormats3", Granularity.SECONDS);
+			put("ListRecords1", Granularity.DAY);
+			put("ListRecords2", Granularity.SECONDS);
+			put("ListRecords3", Granularity.SECONDS);
+			put("ListSets1", Granularity.SECONDS);
+			put("ListSets2", Granularity.SECONDS);
+		}
+	};
+	private static final String EXAMPLE = "/com/github/thanospapapetrou/nefeli/oai/pmh/examples/%1$s.xml";
 
 	@DataProvider(name = "marshalUnmarshal")
 	private static Iterator<Object[]> testMarshalUnmarshalData() throws URISyntaxException {
-		final Iterator<File> examples = Arrays.asList(new File(OaiPmhTest.class.getResource(EXAMPLES).toURI()).listFiles()).iterator();
+		final Iterator<Map.Entry<String, Granularity>> examples = EXAMPLES.entrySet().iterator();
 		return new Iterator<Object[]>() {
 			@Override
 			public boolean hasNext() {
@@ -34,7 +57,8 @@ public class OaiPmhTest {
 
 			@Override
 			public Object[] next() {
-				return new Object[] {examples.next()};
+				final Map.Entry<String, Granularity> example = examples.next();
+				return new Object[] {OaiPmhTest.class.getResource(String.format(EXAMPLE, example.getKey())), example.getValue()};
 			}
 
 			@Override
@@ -49,16 +73,18 @@ public class OaiPmhTest {
 	 * 
 	 * @param example
 	 *            the example to test with
+	 * @param granularity
+	 *            the granularity to test with
 	 * @throws IOException
 	 *             if any errors occur
 	 */
 	@Test(dataProvider = "marshalUnmarshal")
-	public void testMarshalUnmarshal(final File example) throws IOException {
-		try (final InputStream inputStream = new FileInputStream(example)) {
-			final OaiPmh oaiPmh = OaiPmh.unmarshal(inputStream, Granularity.DAY);
+	public void testMarshalUnmarshal(final URL example, final Granularity granularity) throws IOException {
+		try (final InputStream inputStream = example.openStream()) {
+			final OaiPmh oaiPmh = OaiPmh.unmarshal(inputStream, granularity);
 			final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			oaiPmh.marshal(outputStream, Granularity.DAY);
-			Assert.assertEquals(OaiPmh.unmarshal(new ByteArrayInputStream(outputStream.toByteArray()), Granularity.DAY), oaiPmh);
+			oaiPmh.marshal(outputStream, granularity);
+			Assert.assertEquals(OaiPmh.unmarshal(new ByteArrayInputStream(outputStream.toByteArray()), granularity), oaiPmh);
 		}
 	}
 }
