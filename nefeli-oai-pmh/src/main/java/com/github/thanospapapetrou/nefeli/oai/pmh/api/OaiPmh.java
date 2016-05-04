38b7946c0c5c6c6c32c6fcf70c3d875bf2cc12e6
thanos <thanos.papapetrou@gmail.com>
2016-05-05 01:12:37 +0300

    Fixed OAI-PMH client with message body reader

--- nefeli-oai-pmh/src/main/java/com/github/thanospapapetrou/nefeli/oai/pmh/api/OaiPmh.java
+++ nefeli-oai-pmh/src/main/java/com/github/thanospapapetrou/nefeli/oai/pmh/api/OaiPmh.java
@@ -17,99 +17,152 @@ import com.github.thanospapapetrou.nefeli.oai.pmh.domain.SetSpec;
 
 /**
  * Interface representing the OAI-PMH protocol.
+ * 
  * @author thanos
- *
+ * @see <a href="https://www.openarchives.org/OAI/openarchivesprotocol.html">The Open Archives Initiative Protocol for Metadata Harvesting</a>
  */
 public interface OaiPmh {
 	/**
 	 * Perform an OAI-PMH <code>Identify</code> request.
+	 * 
 	 * @return the <code>Identify</code> OAI-PMH response element
-	 * @throws HTTPException if any HTTP error occurs
-	 * @throws InterruptedException if current thread is interrupted while waiting for the OAI-PMH response
-	 * @throws IOException if any I/O error occurs
-	 * @throws OaiPmhException if any OAI-PMH errors occur
+	 * @throws HTTPException
+	 *             if any HTTP error occurs
+	 * @throws InterruptedException
+	 *             if current thread is interrupted while waiting for the OAI-PMH response
+	 * @throws IOException
+	 *             if any I/O error occurs
+	 * @throws OaiPmhException
+	 *             if any OAI-PMH errors occur
 	 */
 	public Identify identify() throws HTTPException, InterruptedException, IOException, OaiPmhException;
 
 	/**
 	 * Perform an OAI-PMH <code>ListMetadataFormats</code> request.
-	 * @param identifier the <code>identifier</code> request argument or <code>null</code> to leave it unspecified
+	 * 
+	 * @param identifier
+	 *            the <code>identifier</code> request argument or <code>null</code> to leave it unspecified
 	 * @return the <code>ListMetadataFormats</code> OAI-PMH response element
-	 * @throws HTTPException if any HTTP error occurs
-	 * @throws InterruptedException if current thread is interrupted while waiting for the OAI-PMH response
-	 * @throws IOException if any I/O error occurs
-	 * @throws OaiPmhException if any OAI-PMH errors occur
+	 * @throws HTTPException
+	 *             if any HTTP error occurs
+	 * @throws InterruptedException
+	 *             if current thread is interrupted while waiting for the OAI-PMH response
+	 * @throws IOException
+	 *             if any I/O error occurs
+	 * @throws OaiPmhException
+	 *             if any OAI-PMH errors occur
 	 */
 	public ListMetadataFormats listMetadataFormats(final URI identifier) throws HTTPException, InterruptedException, IOException, OaiPmhException;
 
 	/**
 	 * Perform an OAI-PMH <code>ListSets</code> request with no <code>resumptionToken</code> argument.
+	 * 
 	 * @return the <code>ListSets</code> OAI-PMH response element
-	 * @throws HTTPException if any HTTP error occurs
-	 * @throws InterruptedException if current thread is interrupted while waiting for the OAI-PMH response
-	 * @throws IOException if any I/O error occurs
-	 * @throws OaiPmhException if any OAI-PMH errors occur
+	 * @throws HTTPException
+	 *             if any HTTP error occurs
+	 * @throws InterruptedException
+	 *             if current thread is interrupted while waiting for the OAI-PMH response
+	 * @throws IOException
+	 *             if any I/O error occurs
+	 * @throws OaiPmhException
+	 *             if any OAI-PMH errors occur
 	 */
 	public ListSets listSets() throws HTTPException, InterruptedException, IOException, OaiPmhException;
 
 	/**
-	 * Perform an OAI-PMH <code>ListSets</code> request with a <code>resumptionToken</code> argument. 
-	 * @param resumptionToken the <code>resumptionToken</code> request argument
+	 * Perform an OAI-PMH <code>ListSets</code> request with a <code>resumptionToken</code> argument.
+	 * 
+	 * @param resumptionToken
+	 *            the <code>resumptionToken</code> request argument
 	 * @return the <code>ListSets</code> OAI-PMH response element
-	 * @throws HTTPException if any HTTP error occurs
-	 * @throws InterruptedException if current thread is interrupted while waiting for the OAI-PMH response
-	 * @throws IOException if any I/O error occurs
-	 * @throws OaiPmhException if any OAI-PMH errors occur
+	 * @throws HTTPException
+	 *             if any HTTP error occurs
+	 * @throws InterruptedException
+	 *             if current thread is interrupted while waiting for the OAI-PMH response
+	 * @throws IOException
+	 *             if any I/O error occurs
+	 * @throws OaiPmhException
+	 *             if any OAI-PMH errors occur
 	 */
 	public ListSets listSets(final String resumptionToken) throws HTTPException, InterruptedException, IOException, OaiPmhException;
 
 	/**
 	 * Perform an OAI-PMH <code>ListIdentifiers</code> request with no <code>resumptionToken</code> argument.
-	 * @param metadataPrefix the <code>metadataPrefix</code> request argument
-	 * @param from the <code>from</code> request argument or <code>null</code> to leave it unspecified
-	 * @param until the <code>until</code> request argument or <code>null</code> to leave it unspecified
-	 * @param set the <code>set</code> request argument or <code>null</code> to leave it unspecified
+	 * 
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> request argument
+	 * @param from
+	 *            the <code>from</code> request argument or <code>null</code> to leave it unspecified
+	 * @param until
+	 *            the <code>until</code> request argument or <code>null</code> to leave it unspecified
+	 * @param set
+	 *            the <code>set</code> request argument or <code>null</code> to leave it unspecified
 	 * @return the <code>ListIdentifiers</code> OAI-PMH response element
-	 * @throws HTTPException if any HTTP error occurs
-	 * @throws InterruptedException if current thread is interrupted while waiting for the OAI-PMH response
-	 * @throws IOException if any I/O error occurs
-	 * @throws OaiPmhException if any OAI-PMH errors occur
+	 * @throws HTTPException
+	 *             if any HTTP error occurs
+	 * @throws InterruptedException
+	 *             if current thread is interrupted while waiting for the OAI-PMH response
+	 * @throws IOException
+	 *             if any I/O error occurs
+	 * @throws OaiPmhException
+	 *             if any OAI-PMH errors occur
 	 */
 	public ListIdentifiers listIdentifiers(final MetadataPrefix metadataPrefix, final Date from, final Date until, final SetSpec set) throws HTTPException, InterruptedException, IOException, OaiPmhException;
 
 	/**
 	 * Perform an OAI-PMH <code>ListIdentifiers</code> request with a <code>resumptionToken</code> argument.
-	 * @param resumptionToken the <code>resumptionToken</code> request argument
+	 * 
+	 * @param resumptionToken
+	 *            the <code>resumptionToken</code> request argument
 	 * @return the <code>ListIdentifiers</code> OAI-PMH response element
-	 * @throws HTTPException if any HTTP error occurs
-	 * @throws InterruptedException if current thread is interrupted while waiting for the OAI-PMH response
-	 * @throws IOException if any I/O error occurs
-	 * @throws OaiPmhException if any OAI-PMH errors occur
+	 * @throws HTTPException
+	 *             if any HTTP error occurs
+	 * @throws InterruptedException
+	 *             if current thread is interrupted while waiting for the OAI-PMH response
+	 * @throws IOException
+	 *             if any I/O error occurs
+	 * @throws OaiPmhException
+	 *             if any OAI-PMH errors occur
 	 */
 	public ListIdentifiers listIdentifiers(final String resumptionToken) throws HTTPException, InterruptedException, IOException, OaiPmhException;
 
 	/**
 	 * Perform an OAI-PMH <code>ListRecords</code> request with no <code>resumptionToken</code> argument.
-	 * @param metadataPrefix the <code>metadataPrefix</code> request argument
-	 * @param from the <code>from</code> request argument or <code>null</code> to leave it unspecified
-	 * @param until the <code>until</code> request argument or <code>null</code> to leave it unspecified
-	 * @param set the <code>set</code> request argument or <code>null</code> to leave it unspecified
+	 * 
+	 * @param metadataPrefix
+	 *            the <code>metadataPrefix</code> request argument
+	 * @param from
+	 *            the <code>from</code> request argument or <code>null</code> to leave it unspecified
+	 * @param until
+	 *            the <code>until</code> request argument or <code>null</code> to leave it unspecified
+	 * @param set
+	 *            the <code>set</code> request argument or <code>null</code> to leave it unspecified
 	 * @return the <code>ListRecords</code> OAI-PMH response element
-	 * @throws HTTPException if any HTTP error occurs
-	 * @throws InterruptedException if current thread is interrupted while waiting for the OAI-PMH response
-	 * @throws IOException if any I/O error occurs
-	 * @throws OaiPmhException if any OAI-PMH errors occur
+	 * @throws HTTPException
+	 *             if any HTTP error occurs
+	 * @throws InterruptedException
+	 *             if current thread is interrupted while waiting for the OAI-PMH response
+	 * @throws IOException
+	 *             if any I/O error occurs
+	 * @throws OaiPmhException
+	 *             if any OAI-PMH errors occur
 	 */
 	public ListRecords listRecords(final MetadataPrefix metadataPrefix, final Date from, final Date until, final SetSpec set) throws HTTPException, InterruptedException, IOException, OaiPmhException;
 
 	/**
 	 * Perform an OAI-PMH <code>ListRecords</code> request with a <code>resumptionToken</code> argument.
-	 * @param resumptionToken the <code>resumptionToken</code> request argument
+	 * 
+	 * @param resumptionToken
+	 *            the <code>resumptionToken</code> request argument
 	 * @return the <code>ListRecords</code> OAI-PMH response element
-	 * @throws HTTPException if any HTTP error occurs
-	 * @throws InterruptedException if current thread is interrupted while waiting for the OAI-PMH response
-	 * @throws IOException if any I/O error occurs
-	 * @throws OaiPmhException if any OAI-PMH errors occur
+	 * @throws HTTPException
+	 *             if any HTTP error occurs
+	 * @throws InterruptedException
+	 *             if current thread is interrupted while waiting for the OAI-PMH response
+	 * @throws IOException
+	 *             if any I/O error occurs
+	 * @throws OaiPmhException
+	 *             if any OAI-PMH errors occur
 	 */
 	public ListRecords listRecords(final String resumptionToken) throws HTTPException, InterruptedException, IOException, OaiPmhException;
 
